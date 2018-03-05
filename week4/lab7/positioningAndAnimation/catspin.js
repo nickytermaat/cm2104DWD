d345ceec61c7abdd87d971b6438e0e55970e2089
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-03-05 16:55:31 +0000

    Week 4 Lab 7 Catspin

--- week4/lab7/positioningAndAnimation/catspin.js
+++ week4/lab7/positioningAndAnimation/catspin.js
@@ -2,6 +2,7 @@
  * Created by john on 10/03/2016.
  */
 
+/**
 var cat = document.querySelector("img");
 var angle = 0, lastTime = null;
 function animate(time) {
@@ -13,5 +14,44 @@ function animate(time) {
     requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
+*/
 
+var context;
+var x=0;
+var y=0;
+var dx=5;
+var dy=5;
 
+function init() {
+    context= catCanvas.getContext('2d');
+    setInterval(drawImage,10);
+}
+function drawImage() {
+    var canvas = document.getElementById("catCanvas");
+    context.clearRect(0,0,1000,350)
+    var img = document.getElementById("catImage");
+    context.drawImage(img,x,y,50,50);
+
+    //Boundary Logic
+    if(x<0 || x>1000) dx=-dx;
+    if(y<0 || y>350) dy=-dy;
+    x+=dx;
+    y+=dy;
+}
+
+//
+// function draw() {
+//   context.clearRect(0,0,1000,350)
+//   context.beginPath();
+//   context.fillStyle="#0000ff";
+//   // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
+//   context.arc(x,y,20,0,Math.PI*2,true);
+//   context.closePath();
+//   context.fill();
+//
+//   //Boundary Logic
+//   if(x<0 || x>1000) dx=-dx;
+//   if(y<0 || y>350) dy=-dy;
+//   x+=dx;
+//   y+=dy;
+// }
