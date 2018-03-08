4be11cb4a93cb625266eef4da500d39c5f21c0a6
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-03-08 10:41:31 +0000

    Week 5 Lab 9 squares

--- week5/Lab9/Lab9/squares.js
+++ week5/Lab9/Lab9/squares.js
@@ -1,6 +1,6 @@
 $(function(){
 	// usual main starting point when web page loads
-	
+
 	// simple CSS class switcher
 	// find all divs
 	// bind to click events
@@ -8,15 +8,42 @@ $(function(){
 	$("div").click(function(){
 		// $(this) is a shortcut for the element we just selected
 		// using $("div")
+
 		if ($(this).hasClass("red")) {
-			$(this).addClass("blue").removeClass("red");
+			$(this).toggleClass("red blue");
+			console.log("R - RB -> B")
 		}
 		else if ($(this).hasClass("blue")) {
-			$(this).addClass("green").removeClass("blue");
+			$(this).toggleClass("blue green");
+			console.log("B - BG -> G")
 		}
 		else if ($(this).hasClass("green")) {
-			$(this).addClass("red").removeClass("green");
+			$(this).toggleClass("green red");
+			console.log("G - GR -> R")
 		}
+
+		// if ($(this).hasClass("red") && !$(this).hasClass("red blue")) {
+		// 	$(this).toggleClass("blue");
+		// 	console.log("Red->Blue " + " changed colour " + $(this).hasClass("red blue"));
+		// }
+		// else if (($(this).hasClass("blue")) || ($(this).hasClass("red blue"))) {
+		// 	$(this).toggleClass("green");
+		// 	console.log("Blue->Green " + $(this).toggleClass("blue green"));
+		// }
+		// else if ($(this).hasClass("green")) {
+		// 	$(this).toggleClass("red green blue");
+		// 	console.log("Green-> " + $(this).toggleClass("green"));
+		// }
 	});
-	
 });
+
+	// 	if ($(this).hasClass("red")) {
+	// 		$(this).addClass("blue").removeClass("red");
+	// 	}
+	// 	else if ($(this).hasClass("blue")) {
+	// 		$(this).addClass("green").removeClass("blue");
+	// 	}
+	// 	else if ($(this).hasClass("green")) {
+	// 		$(this).addClass("red").removeClass("green");
+	// 	}
+	// });
