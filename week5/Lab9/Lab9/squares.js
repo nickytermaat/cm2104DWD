215825288d7e1649260dc710329153d7cf305cd6
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-03-12 15:07:41 +0000

    Week 5 Lab 9 finished

--- week5/Lab9/Lab9/squares.js
+++ week5/Lab9/Lab9/squares.js
@@ -21,29 +21,5 @@ $(function(){
 			$(this).toggleClass("green red");
 			console.log("G - GR -> R")
 		}
-
-		// if ($(this).hasClass("red") && !$(this).hasClass("red blue")) {
-		// 	$(this).toggleClass("blue");
-		// 	console.log("Red->Blue " + " changed colour " + $(this).hasClass("red blue"));
-		// }
-		// else if (($(this).hasClass("blue")) || ($(this).hasClass("red blue"))) {
-		// 	$(this).toggleClass("green");
-		// 	console.log("Blue->Green " + $(this).toggleClass("blue green"));
-		// }
-		// else if ($(this).hasClass("green")) {
-		// 	$(this).toggleClass("red green blue");
-		// 	console.log("Green-> " + $(this).toggleClass("green"));
-		// }
 	});
 });
-
-	// 	if ($(this).hasClass("red")) {
-	// 		$(this).addClass("blue").removeClass("red");
-	// 	}
-	// 	else if ($(this).hasClass("blue")) {
-	// 		$(this).addClass("green").removeClass("blue");
-	// 	}
-	// 	else if ($(this).hasClass("green")) {
-	// 		$(this).addClass("red").removeClass("green");
-	// 	}
-	// });
