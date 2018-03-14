3a5f4cd27613e5ca753ec3dda7408a63157643df
Nicky <nickytermaat24051996@live.nl>
2018-03-14 14:15:11 +0100

    Week 5 Lab 10 Rover earth_date

--- week5/lab10/mars-example/mars-example/roverparser.js
+++ week5/lab10/mars-example/mars-example/roverparser.js
@@ -25,7 +25,8 @@
   {
     output+="<tr><td>" + photos[i].rover.name + "</td><td>"
     + photos[i].camera.full_name + "</td><td>"
-    + photos[i].img_src + "</td></tr>";
+    + photos[i].img_src + "</td><td>";
+    + photos[i].earth_date + "</td></tr>"
   }
   output+="</tbody></table>";
 
