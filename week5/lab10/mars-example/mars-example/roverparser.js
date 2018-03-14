bc9089ad1bdc102ec4df16959325620864aa4a84
Nicky <nickytermaat24051996@live.nl>
2018-03-14 14:31:34 +0100

    Week 5 Lab 10 Show image instead of url

--- week5/lab10/mars-example/mars-example/roverparser.js
+++ week5/lab10/mars-example/mars-example/roverparser.js
@@ -25,7 +25,7 @@
   {
     output+="<tr><td>" + photos[i].rover.name + "</td><td>"
     + photos[i].camera.full_name + "</td><td>"
-    + photos[i].img_src + "</td><td>"
+    + "<img src=" + photos[i].img_src + "></td><td>"
     + photos[i].earth_date + "</td><td>"
     + photos[i].rover.launch_date +  "</td></tr>";
   }
