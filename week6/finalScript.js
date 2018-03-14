091b00665ca790b0fcaab85af6e40ac352f14c10
Nicky <nickytermaat24051996@live.nl>
2018-03-14 18:39:13 +0100

    Week 6 Lab 11 Final Script

---
+++ week6/finalScript.js
@@ -0,0 +1,26 @@
+$(function() {
+  alert("Document ready prettyPrintJSON");
+
+  $('#searchform').submit(function() {
+    var searchterms = $("#searchterms").val();
+    getResultsFromOMDB(searchterms);
+    return false;
+  });
+});
+
+function getResultsFromOMDB(searchterms) {
+  var url = "http://www.omdbapi.com/?apikey=ced0a703&s=" + searchterms;
+  $.getJSON(url, function(jsondata) {
+    addResultTitels(jsondata);
+  });
+}
+
+function addResultTitels(jsondata) {
+  var htmlstring = "";
+  for (var i = 0; i < 10; i++) {
+    var title = jsondata.Search[i].Title;
+    htmlstring += "<li>" + title + "</title>";
+  }
+
+  $("#results").html(htmlstring);
+}
