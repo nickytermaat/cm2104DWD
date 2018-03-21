85ee2e1499deac6e4e1e1d41a8f502e13bb2f4ce
Nicky <nickytermaat24051996@live.nl>
2018-03-21 22:13:15 +0100

    Week 7 Lab 13 datetime and secondserver

---
+++ week7/lab13/mysecondserver.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var currentDate = requre('./mymodule');
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + currentDate.myDateTime());
+  res.end('Hello World');
+}).listen(8080);
