3e729fa5bace7eadb3a4e08b4a4fb61f8dadf15e
Nicky <nickytermaat24051996@live.nl>
2018-03-21 12:55:13 +0100

    Week 6 Lab 11 Ratings & plot undefined

--- week6/ExpansionFinalScript.js
+++ week6/ExpansionFinalScript.js
@@ -17,15 +17,16 @@ function getResultsFromOMDB(searchterms) {
 }
 
 function addResultTitels(jsondata) {
-  var htmlstring = "<table><thead><tr><th>Poster</th><th>Film title</th><th>Release year</th><th>Type</th><th>IMDB page</th><th>Rotten Tomatoes</th></thead><tbody>";
+  var htmlstring = "<table><thead><tr><th>Poster</th><th>Film title</th><th>Release year</th><th>Type</th><th>IMDB page</th><th>Rotten Tomatoes</th><th>Plot</th></thead><tbody>";
   for (var i = 0; i < 5; i++) {
     if (jsondata.Search[i].Type == "movie") {
       htmlstring += "<tr><td>" + "<img src=" + jsondata.Search[i].Poster + "></td><td>";
       htmlstring += jsondata.Search[i].Title + "</td><td>";
       htmlstring += jsondata.Search[i].Year + "</td><td>";
       htmlstring += jsondata.Search[i].Type + "</td><td>";
-      htmlstring += "<a href=\"http://www.imdb.com/title/" + jsondata.Search[i].imdbID + "\">" + jsondata.Search[i].Title + "</a>" + "</td></tr>";
-      console.log("Rotten Tomatoes " +jsondata.Search[i].Ratings[i].);
+      htmlstring += "<a href=\"http://www.imdb.com/title/" + jsondata.Search[i].imdbID + "\">" + jsondata.Search[i].Title + "</a>" + "</td><td>";
+      htmlstring += jsondata.Search[i].Ratings + "</td><td>"; // undefined?
+      htmlstring += jsondata.Search[i].Plot + "</td></tr>"; //undefined?
     }
   }
   htmlstring += "</tbody></table>";
