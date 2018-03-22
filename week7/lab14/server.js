4cdbef961934f38422ee4937c9cac5ef1e02c61b
Nicky <nickytermaat24051996@live.nl>
2018-03-22 21:12:01 +0100

    Week 7 Lab 14 comment out code

--- week7/lab14/server.js
+++ week7/lab14/server.js
@@ -9,26 +9,26 @@ var client = new Twitter({
 });
 
 app.use(express.static('public'))
-
-app.get('/', function(req, res) {
-  var params = {screen_name: 'nodejs'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if(!error) {
-      // console.log(tweets);
-      // res.send(tweets);
-      var output = "";
-      for (var t = 0; t <tweets.length; t++) {
-        output += "<div>";
-        output += "<h2>" + tweets[t].user.screen_name + "</h2>";
-        output += "<p>" + tweets[t].text + "</p>";
-        output += "</div>";
-      }
-      res.send(output);
-    } else {
-      res.send("Hello World! by express");
-    }
-  });
-});
+//
+// app.get('/', function(req, res) {
+//   var params = {screen_name: 'nodejs'};
+//   client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//     if(!error) {
+//       // console.log(tweets);
+//       // res.send(tweets);
+//       var output = "";
+//       for (var t = 0; t <tweets.length; t++) {
+//         output += "<div>";
+//         output += "<h2>" + tweets[t].user.screen_name + "</h2>";
+//         output += "<p>" + tweets[t].text + "</p>";
+//         output += "</div>";
+//       }
+//       res.send(output);
+//     } else {
+//       res.send("Hello World! by express");
+//     }
+//   });
+// });
 
 app.get('/search', function(req, res) {
   var searchquery = req.query.q;
