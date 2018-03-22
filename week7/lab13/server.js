01836c4f1d4914093f425adfebcb96da3fb9d314
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-03-22 10:23:08 +0000

    Week 7 Lab 13 fix typo

--- week7/lab13/server.js
+++ week7/lab13/server.js
@@ -37,7 +37,7 @@ app.get('/calc', function(req, res){
 });
 
 app.use(express.static('public'));
-ap.get('/getform', function(req, res) {
+app.get('/getform', function(req, res) {
   var name = req.query.name;
   var quest = req.query.quest;
   res.send("Hi " + name + ", I am sure you will " + quest);
