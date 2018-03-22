b7eee8d40f369eee58baab068c21ab0863e0ad79
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-03-22 10:58:57 +0000

    Week 7 Lab 14 requiring twitter

--- week7/lab14/server.js
+++ week7/lab14/server.js
@@ -1,7 +1,22 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
+var Twitter = require('twitter');
+var client = new Twitter({
+  consumer_key: '	7dkFLKUogbmVY6y35HRKvXshu',
+  consumer_secret: 'BqZx2DdrISAbVhbSOUXbZcKpcsyjIDUKC54U8k16MGBVUezakB',
+  access_token_key: '305058684-RFAP9w3a3ML6Fz6WBwvzeF1Yy34dbrPuKRT8rRQX',
+  access_token_secret: 'sA8WK5tlitIFqRxxbQgHi8SdGF9IXDH3tBRoq4fdGW1DI'
+});
 
+
+
+
+
+
+
+
+
+app.use(express.static('public'));
 app.get('/', function(req, res) {
   res.send("Hello World! by express");
 });
