a999713be16e2b13acc2840b35a0bbe6065b5b00
Nicky <nickytermaat24051996@live.nl>
2018-03-22 21:11:25 +0100

    Week 7 Lab 14 search and status

--- week7/lab14/server.js
+++ week7/lab14/server.js
@@ -10,45 +10,51 @@ var client = new Twitter({
 
 app.use(express.static('public'))
 
-// app.get('/', function(req, res) {
-//   var params = {screen_name: 'nodejs'};
-//   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//     if(!error) {
-//       // console.log(tweets);
-//       // res.send(tweets);
-//       var output = "";
-//       for (var t = 0; t <tweets.length; t++) {
-//         output += "<div>";
-//         output += "<h2>" + tweets[t].user.screen_name + "</h2>";
-//         output += "<p>" + tweets[t].text + "</p>";
-//         output += "</div>";
-//       }
-//       res.send(output);
-//     } else {
-//       res.send("Hello World! by express");
-//     }
-//   });
-// });
+app.get('/', function(req, res) {
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if(!error) {
+      // console.log(tweets);
+      // res.send(tweets);
+      var output = "";
+      for (var t = 0; t <tweets.length; t++) {
+        output += "<div>";
+        output += "<h2>" + tweets[t].user.screen_name + "</h2>";
+        output += "<p>" + tweets[t].text + "</p>";
+        output += "</div>";
+      }
+      res.send(output);
+    } else {
+      res.send("Hello World! by express");
+    }
+  });
+});
 
 app.get('/search', function(req, res) {
   var searchquery = req.query.q;
   console.log("Searchquery: " + searchquery);
-  var params = {q: searchquery};
-  client.get('search/tweets', params, function(error, tweets, response) {
+  client.get('search/tweets', {q: searchquery}, function(error, tweets, response) {
     if(!error) {
-      console.log(tweets);
-      console.log("Amount of tweets: " + tweets.length);
-      var output = "";
-      for (var t = 0; t <tweets.length; t++) {
-        output += "<div>";
-        //output += "<h2>" + tweets[t].user.screen_name + "</h2>";
-        output += "<p>" + tweets[t].text + "</p>";
-        output += "</div>";
-      }
-      console.log(output);
-      res.send(output);
+      var params = {screen_name: searchquery};
+      client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if(!error) {
+          console.log(tweets);
+          // res.send(tweets);
+          var output = "";
+          for (var t = 0; t <tweets.length; t++) {
+            output += "<div>";
+            output += "<h2>" + tweets[t].user.screen_name + "</h2>";
+            output += "<p>" + tweets[t].text + "</p>";
+            output += "</div>";
+          }
+          console.log("Output ready");
+          res.send(output);
+        } else {
+          res.send("Fail: statuses/user_timeline");
+        }
+      });
     } else {
-      res.send("Hello World! by express");
+      res.send("Fail: search/tweets")
     }
   });
 });
