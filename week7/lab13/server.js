d44b0851df9ab70bfc010a9ec9e2d3b6bb9332b9
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-03-22 09:36:25 +0000

    Week 7 Lab 13 Route 2

--- week7/lab13/server.js
+++ week7/lab13/server.js
@@ -1,7 +1,14 @@
 var express = require('express')
 var app = express();
+
+
 app.get('/', function(req, res){
   res.send("Hello World! by express");
 });
+
+app.get('/test'. function(req, res) {
+  res.send("This is route 2 /test");
+});
+
 
 app.listen(8080);
