00f2a6f28a642c26cb7e48fc8b2ff68aa4774e1c
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-03-22 09:42:09 +0000

    Week 7 Lab 13 Joke

--- week7/lab13/server.js
+++ week7/lab13/server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express();
 
+var knockknock = require('knock-knock-jokes');
+
 
 app.get('/', function(req, res){
   res.send("Hello World! by express");
@@ -10,5 +12,9 @@ app.get('/test', function(req, res){
   res.send("This is route 2 /test");
 });
 
+app.get('/joke', function(req, res){
+  var randomJoke = knockknock();
+  res.send(randomJoke);
+});
 
 app.listen(8080);
